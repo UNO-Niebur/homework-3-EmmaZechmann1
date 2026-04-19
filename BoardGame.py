5690abc2d813e1a0043388dac48bdf865a6b37c4
EmmaZechmann1 <90341132@nebraska.edu>
2026-04-19 18:47:51 +0000

    Done

--- BoardGame.py
+++ BoardGame.py
@@ -1,39 +1,74 @@
 # Homework 3 - Board Game System
-# Name:
-# Date:
+# Name: Emma Zechmann
+# Date: 4/19/26
+import random 
 
 def loadGameData(filename):
-    """Reads game data from a file and returns it as a list."""
-    data = []
+    data = {}
+    turn = None
+
     with open(filename, "r") as file:
         for line in file:
-            data.append(line.strip())
-    return data
+            line = line.strip()
 
+            if line.startswith("Turn:"):
+                turn = line.split(":")[1].strip()
+            
+            elif ":" in line:
+                position, value = line.split(":")
+                data[int(position)] = value.strip()
 
-def displayGame(data):
-    """Displays the current game state."""
-    print("\nCurrent Game State:")
-    for item in data:
-        print(item)
+    return turn, data
 
+def displayGame(turn, data):
 
-def movePlayer(data):
-    """Example function to simulate moving a player."""
-    print("\nMove player function not fully implemented.")
-    # Students will modify this
+    print("\n===== Game State ===== ")
+    print("Current Turn:", turn)
+    print("-------------------------")
+
+    for i in range(1,30):
+        if i in data:
+            print(f"{i}: {data[i]}")
+        else: 
+            print(f"{i}: Empty")
+
+def findPlayer(data, player_name):
+    for pos, value in data.items():
+        if value == player_name:
+            return pos
+
+    return None 
+
+def movePlayer(data, turn):
+    pos = findPlayer(data, turn)
+
+    if pos is None:
+        print("player not found!")
+        return
+
+    roll = random.randint(1,6)
+    print(f"\n{turn} rolled a {roll}!")
+
+    new_pos = pos + roll
+
+    print(f"{turn} moves from {pos} to {new_pos}!")
+    del data [pos]
+    data[new_pos] = turn
 
 
 def main():
-    filename = "events.txt"   # Students can rename if needed
+    filename = "events.txt"   
+    turn, gameData = loadGameData(filename)
 
-    gameData = loadGameData(filename)
-    displayGame(gameData)
+    while True:
+        displayGame(turn, gameData)
+        choice = input("\nMove player? (y/n): ").lower()
 
-    # Example interaction
-    choice = input("\nMove player? (y/n): ")
-    if choice.lower() == "y":
-        movePlayer(gameData)
+        if choice == "y":
+            movePlayer(gameData, turn)
+        else: 
+            print("Game Ended! ")
+            break
 
 
 if __name__ == "__main__":
