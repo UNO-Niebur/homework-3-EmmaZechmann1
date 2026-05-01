3c79dab55b7dceab953da37cbb382444e18221b6
EmmaZechmann1 <90341132@nebraska.edu>
2026-05-01 01:22:02 +0000

    rework

--- BoardGame.py
+++ BoardGame.py
@@ -20,13 +20,19 @@ def loadGameData(filename):
 
     return turn, data
 
+def saveGameData(filename, turn, data):
+    with open(filename, "w") as file:
+        file.write(f"Turn: {turn}\n")
+        for pos in data:
+            file.write(f"{pos}:{data[pos]}\n")
+
 def displayGame(turn, data):
 
     print("\n===== Game State ===== ")
     print("Current Turn:", turn)
     print("-------------------------")
 
-    for i in range(1,30):
+    for i in range(1,100):
         if i in data:
             print(f"{i}: {data[i]}")
         else: 
@@ -43,29 +49,66 @@ def movePlayer(data, turn):
     pos = findPlayer(data, turn)
 
     if pos is None:
-        print("player not found!")
-        return
+        print(f"{turn} not found on board. Placing at start.")
+        pos = 1
+        data[pos] = turn
 
     roll = random.randint(1,6)
     print(f"\n{turn} rolled a {roll}!")
 
     new_pos = pos + roll
 
+    if new_pos == 10:
+        print("You landed on the bridge shortcut! Move forward 8 spaces.")
+        new_pos +=8
+
+    if new_pos == 15:
+        print("You landed on an angry troll and he pushes you back three spaces!")
+        new_pos -=3
+
     print(f"{turn} moves from {pos} to {new_pos}!")
+
     del data [pos]
     data[new_pos] = turn
+
+    if new_pos >= 100:
+        print(f"\n {turn} made it to the castle and won the game!")
+        return True
+    
+    return False 
 
 
 def main():
     filename = "events.txt"   
     turn, gameData = loadGameData(filename)
 
+    players = ["Player1", "Player2"]
+
+    if "Player1" not in gameData.values():
+        gameData[1] = "Player1"
+
+    if "Player2" not in gameData.values():
+        gameData[1] = "Player2"
+
     while True:
         displayGame(turn, gameData)
         choice = input("\nMove player? (y/n): ").lower()
 
         if choice == "y":
-            movePlayer(gameData, turn)
+            won = movePlayer(gameData, turn)
+
+            saveGameData(filename, turn, gameData)
+
+            if won:
+                break
+
+            current_index = players.index(turn)
+            current_index = (current_index +1) % len(players)
+            turn = players[current_index]
+
+        
+
+
         else: 
             print("Game Ended! ")
             break
